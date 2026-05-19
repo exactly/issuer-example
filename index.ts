6cce2b6c4dc9d6bcb50c0d89c62e04e9ced52ec3
danilo neves cruz <cruzdanilo@gmail.com>
2026-05-19 18:34:08 -0300

    ✨ implement webauthn flow

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
-import { hexToBigInt } from "viem";
+import { encodeAbiParameters, hashTypedData, hexToBigInt, type TypedData, type TypedDataDefinition } from "viem";
 import { generatePrivateKey, privateKeyToAccount, type LocalAccount } from "viem/accounts";
 import { optimismSepolia } from "viem/chains";
+import { P256, WebAuthnP256 } from "ox";
 
 import { createMessage, signMessage } from "./exa-client";
 import { generateNonce, host, verifyMessage } from "./issuer-server";
@@ -11,6 +12,8 @@ const last4 = "1234";
 const userId = "0a1b-0000-0000-0000-000000000000";
 
 await flow(siweOwner());
+console.log("\n\n");
+await flow(webauthnOwner());
 
 async function flow({ owner, x, y }: { owner: Pick<LocalAccount, "signTypedData">; x: bigint; y: bigint }) {
   const nonce = generateNonce({ userId }); // issuer server
@@ -27,4 +30,50 @@ async function flow({ owner, x, y }: { owner: Pick<LocalAccount, "signTypedData"
 function siweOwner() {
   const owner = privateKeyToAccount(generatePrivateKey());
   return { owner, x: hexToBigInt(owner.address), y: 0n };
+}
+
+function webauthnOwner() {
+  const privateKey = P256.randomPrivateKey();
+  return {
+    ...P256.getPublicKey({ privateKey }),
+    owner: {
+      signTypedData: async <
+        const typedData extends TypedData | Record<string, unknown>,
+        primaryType extends keyof typedData | "EIP712Domain" = keyof typedData,
+      >(
+        typedData: TypedDataDefinition<typedData, primaryType>,
+      ) => {
+        const { metadata, payload } = WebAuthnP256.getSignPayload({
+          challenge: encodeAbiParameters([{ type: "bytes32" }], [hashTypedData(typedData)]),
+          userVerification: "required",
+          origin: `https://${host}`,
+          rpId: host,
+        });
+        return encodeAbiParameters(
+          [
+            {
+              type: "tuple",
+              components: [
+                { name: "authenticatorData", type: "bytes" },
+                { name: "clientDataJSON", type: "string" },
+                { name: "challengeIndex", type: "uint256" },
+                { name: "typeIndex", type: "uint256" },
+                { name: "r", type: "uint256" },
+                { name: "s", type: "uint256" },
+              ],
+            },
+          ],
+          [
+            {
+              authenticatorData: metadata.authenticatorData,
+              clientDataJSON: metadata.clientDataJSON,
+              challengeIndex: BigInt(metadata.challengeIndex ?? 0),
+              typeIndex: BigInt(metadata.typeIndex ?? 0),
+              ...P256.sign({ payload, privateKey, hash: true }),
+            },
+          ],
+        );
+      },
+    },
+  };
 }
